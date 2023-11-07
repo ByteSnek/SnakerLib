e898b94db8df6572fe83109592fde8d71fa5654d
SnakerBone <raxxtq@gmail.com>
2023-11-07 20:25:31 +1100

    1.7.7

--- src/main/java/xyz/snaker/snakerlib/utility/ItemProperties.java
+++ src/main/java/xyz/snaker/snakerlib/utility/ItemProperties.java
@@ -5,23 +5,20 @@ import net.minecraft.world.item.Item;
 /**
  * Created by SnakerBone on 16/08/2023
  **/
-@FunctionalInterface
 public interface ItemProperties
 {
     /**
      * Empty item properties instance
      **/
-    ItemProperties EMPTY = (durability) -> new Item.Properties().defaultDurability(durability);
+    Item.Properties EMPTY = new Item.Properties();
 
     /**
      * Default properties for a tool item
      **/
-    ItemProperties TOOL = (durability) -> new Item.Properties().defaultDurability(durability).stacksTo(1);
+    Item.Properties TOOL = new Item.Properties().stacksTo(1);
 
     /**
      * Default properties for a special tool item (e.g. ender pearl)
      **/
-    ItemProperties SPECIAL = (durability) -> new Item.Properties().defaultDurability(durability).stacksTo(1).setNoRepair();
-
-    Item.Properties apply(int durability);
+    Item.Properties SPECIAL = new Item.Properties().stacksTo(1).setNoRepair();
 }
